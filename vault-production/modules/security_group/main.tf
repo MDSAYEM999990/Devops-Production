abe591fb9e3e50780263049f2b29bde983218ff5
MDSAYEM999990 <128892247+MDSAYEM999990@users.noreply.github.com>
2025-05-27 16:46:03 +0600

    Update main.tf

--- vault-production/modules/security_group/main.tf
+++ vault-production/modules/security_group/main.tf
@@ -6,14 +6,14 @@ resource "aws_security_group" "vault" {
     from_port   = 8200
     to_port     = 8200
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
@@ -23,3 +23,29 @@ resource "aws_security_group" "vault" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+resource "aws_security_group" "endpoints_sg" {
+  name        = "vpce-endpoints-sg"
+  description = "Allow VPC Interface Endpoint traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"] # Only VPC internal access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "vpce-endpoints-sg"
+  }
+}
+
